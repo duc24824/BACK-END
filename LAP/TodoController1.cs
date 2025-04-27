b162dc0d4247502922764165ae7e7b5b58fc3b43
duc24824 <163629699+duc24824@users.noreply.github.com>
2025-04-27 20:49:13 +0700

    Add files via upload

---
+++ LAP/TodoController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lap2.Views
+{
+    public class TodoController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
