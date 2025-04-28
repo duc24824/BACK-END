994b6ff9a7b89635de861e7085dfc23be6733436
duc24824 <163629699+duc24824@users.noreply.github.com>
2025-04-28 09:46:06 +0700

    Add files via upload

---
+++ Lap3/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Lap3.Models;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Lap3.Controllers
+{
+    public class UserController : Controller
+    {
+        static List<User> users = new List<User>()
+        {
+            new User() {id="1",username="aaa",password="1",Phone="111",email="111"},
+            new User() {id="1",username="aaa",password="1",Phone="111",email="111"},
+            new User() {id="1",username="aaa",password="1",Phone="111",email="111"},
+            new User() {id="1",username="aaa",password="1",Phone="111",email="111"},
+            new User() {id="1",username="aaa",password="1",Phone="111",email="111"},
+        };
+        public IActionResult Index()
+        {
+            return View(users);
+        }
+    }
+}
